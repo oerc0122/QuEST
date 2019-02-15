8ba23925acb22bcdc42fde6065cf1c3b4b3c7b8e
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2019-02-15 17:17:06 +0000

    First attempt to separate C/Py

--- QASMParser/QASMToQuest.py
+++ QASMParser/QASMToQuest.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 
-import argparse
 from QASMParser.QASMParser import *
 import QASMParser.QASMQuESTGate
 
-parser = argparse.ArgumentParser(description='QASM parser to translate from QASM to QuEST input', add_help=True)
-parser.add_argument('sources', nargs=argparse.REMAINDER, help="List of sources to compile")
-parser.add_argument('-o','--output', help="File to compile to")
-argList = parser.parse_args()
-
-if len(argList.sources) > 1: raise NotImplementedError('Cannot compile multiple sources')
-if len(argList.sources) == 0:
-    parser.print_help()
-    quit()
+from QASMParser.cli import get_command_args
+argList = get_command_args()
 
 for source in argList.sources:
-    myProg = Prog(source)
+    myProg = ProgFile(source)
 myProg.to_c(argList.output)
