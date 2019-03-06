18eeacb8482306bf6442c42bbbcf90a9e045f5e6
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2019-03-06 15:18:24 +0000

    Working version of ripple add, compiling and running.

--- QASMParser/QASMParser/QASMQuESTGate.py
+++ QASMParser/QASMParser/QASMQuESTGate.py
@@ -1,6 +1,12 @@
 from .QASMParser import *
 from copy import copy
 
+class QuESTArg(Referencable):
+    def __init__(self, name, val, type_):
+        self.name = name
+        self.val = val
+        self.type_ = type_
+
 class QuESTLibGate(Gate):
     def __init__(self, name, cargs, qargs, argOrder, internalName):
         self.type_ = "Gate"
@@ -10,66 +16,70 @@ class QuESTLibGate(Gate):
         self._qargs = self.parse_qarg_string(qargs)
         self.internalName = internalName
         self.argOrder = argOrder
-        Gate.gates[self.name] = self
         Gate.internalGates[self.name] = self
-        
+
     def parse_qarg_string(self, qargString):
         qargs = [ coreTokens.namedQubit(qarg).groups() for qarg in qargString.split(',')]
         qargs = [[arg[0], arg[0]+"_index"] for arg in qargs]
         return qargs
 
-    def reorder_args(self, qargsIn, cargsIn):
-        qargs = copy(qargsIn)
-        cargs = copy(cargsIn)
-        argString = ""
+    def reorder_args(self, qargs, cargs):
         preString = []
         args = []
+        outCargs = []
         rest = []
         for expect in self.argOrder:
             if expect == "nextQureg":
+                args.append('qreg')
                 qarg = qargs.pop(0)
-                args.append(qarg[0].name)
+                # args.append(qarg[0].name)
             elif expect == "index":
-                args.append(str(qarg[1]))
+                args.append(Operation.resolve_arg(self,qarg))
             elif expect == "nextIndex":
                 qarg = qargs.pop(0)
-                args.append(str(qarg[1]))
+                args.append(Operation.resolve_arg(self,qarg))
         if "mConRestIndex" in self.argOrder:
             for qarg in qargs:
-                rest.append(qarg[1])
+                rest += [qarg[1]]
         nTemp = 0
         for arg in self.argOrder:
             if arg == "nextQureg" or arg == "nextIndex" or arg == "index":
-                argString += ", "+args.pop(0)
+                outCargs += [args.pop(0)]
             elif arg == "mConRestIndex":
                 nTemp+=1
                 tempVar = "tmp"+str(nTemp)
                 nRest     = len(rest)
-                preString += [f"int {tempVar}[{nRest}] = {{"+",".join(rest)+"}"]
-                argString += f", (int*){tempVar}, {nRest}"
+                preString += [f"int {tempVar}[{nRest}] = {{"+",".join(rest)+"};"]
+                outCargs += [f"(int*) {tempVar}",f"{nRest}"]
             elif arg == "nextCarg":
-                argString += f"{cargs.pop(0)}"
+                outCargs += [cargs.pop(0)]
             elif arg == "cargs":
-                argString += f", {', '.join(cargs)}"
+                outCargs += cargs
                 cargs = []
             elif arg == "complexCarg":
                 nTemp += 1
                 tempVar = "tmp"+str(nTemp)
                 tempArg = [cargs.pop(0), cargs.pop(0)]
-                preString += [f"Complex {tempVar} = {{{','.join(tempArg)}}}"]
-                argString += f",{tempVar}"
+                preString += [f"Complex {tempVar} = {{{','.join(tempArg)}}};"]
+                outCargs += [tempVar]
             elif arg == "complexMatrix2Carg":
                 nTemp += 1
                 tempVar = "tmp"+str(nTemp)
                 tempArg = [cargs.pop(0), cargs.pop(0), cargs.pop(0), cargs.pop(0)]
-                preString += [f"ComplexMatrix2 {tempVar} = {{{','.join(tempArg)}}}"]
-                argString += f",{tempVar}"
+                preString += [f"ComplexMatrix2 {tempVar} = {{{','.join(tempArg)}}};"]
+                outCargs += [tempVar]
             elif arg == "not":
                 nTemp += 1
                 tempVar = "not"
-                preString += [f"ComplexMatrix2 {tempVar} = {{0., 1., 1., 0.}}"]
-                argString += ","+tempVar
-        return preString, argString.strip(',')
+                preString += [
+                    f"ComplexMatrix2 {tempVar};",
+                    f"{tempVar}.r0c0 = (Complex) {{.real=0., .imag=0.}};",
+                    f"{tempVar}.r0c1 = (Complex) {{.real=1., .imag=0.}};",
+                    f"{tempVar}.r1c0 = (Complex) {{.real=1., .imag=0.}};",
+                    f"{tempVar}.r1c1 = (Complex) {{.real=0., .imag=0.}};"
+                ]
+                outCargs += [tempVar]
+        return preString, outCargs
 
 QuESTLibGate(name = "x",   cargs = None, qargs = "a", argOrder = ("nextQureg", "index"), internalName = "pauliX")
 QuESTLibGate(name = "cx",  cargs = None, qargs = "a", argOrder = ("nextQureg", "index", "nextIndex"), internalName = "controlledNot")
@@ -77,4 +87,3 @@ QuESTLibGate(name = "ccx", cargs = None, qargs = "a,b,c", argOrder = ("nextQureg
 QuESTLibGate(name = "rotateX", cargs = "phi", qargs = "a", argOrder = ("nextQureg", "index", "cargs"), internalName = "rotateX")
 QuESTLibGate(name = "rotateY", cargs = "theta", qargs = "a", argOrder = ("nextQureg", "index", "cargs"), internalName = "rotateY")
 QuESTLibGate(name = "rotateZ", cargs = "lambda", qargs = "a", argOrder = ("nextQureg", "index", "cargs"), internalName = "rotateZ")
-
